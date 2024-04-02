765d48fa9d3ab6a221a3e4d615cab790476ec5c3
isaacmckibbin <161265676+isaacmckibbin@users.noreply.github.com>
2024-04-03 09:42:07 +1300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+import random
+
+options = ("rock", "paper", "scissor")
+player = None
+computer = random.choice(options)
+
+while player not in options:
+    player = input("Enter a choice (rock, paper, scissor): ")
+
+print(f"Player: {player}")
+print(f"computer: {computer}")
+
+if player == computer:
+    print("It's a tie!")
+elif player == "rock" and computer == "scissors":
+    print("You win!")
+elif player == "scissors" and computer == "rock":
+    print("You win!")
+elif player == "scissors" and computer == "paper":
+    print("You win!")
+else:
+    print("You lose!")
+
+import random
+
+options = ("rock", "paper", "scissor")
+player = None
+computer = random.choice(options)
+
+while player not in options:
+    player = input("Enter a choice (rock, paper, scissor): ")
+
+print(f"Player: {player}")
+print(f"computer: {computer}")
+
+if player == computer:
+    print("It's a tie!")
+elif player == "rock" and computer == "scissors":
+    print("You win!")
+elif player == "scissors" and computer == "rock":
+    print("You win!")
+elif player == "scissors" and computer == "paper":
+    print("You win!")
+else:
+    print("You lose!")
+
+
+import random
+
+options = ("rock", "paper", "scissor")
+player = None
+computer = random.choice(options)
+
+while player not in options:
+    player = input("Enter a choice (rock, paper, scissor): ")
+
+print(f"Player: {player}")
+print(f"computer: {computer}")
+
+if player == computer:
+    print("It's a tie!")
+elif player == "rock" and computer == "scissors":
+    print("You win!")
+elif player == "scissors" and computer == "rock":
+    print("You win!")
+elif player == "scissors" and computer == "paper":
+    print("You win!")
+else:
+    print("You lose!")
+
+
+print("thank for playing!")
+print("play again soon!(:")
+print("by Isaac") 
