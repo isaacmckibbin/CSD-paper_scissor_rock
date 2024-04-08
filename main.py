3ae270f65233375362ab5b21e9945e60a1785504
isaacmckibbin <161265676+isaacmckibbin@users.noreply.github.com>
2024-04-08 15:00:21 +1200

    Update main.py

--- main.py
+++ main.py
@@ -19,31 +19,7 @@ elif player == "scissors" and computer == "rock":
 elif player == "scissors" and computer == "paper":
     print("You win!")
 else:
-    print("You win!")
-
-import random
-
-options = ("rock", "paper", "scissor")
-player = None
-computer = random.choice(options)
-
-while player not in options:
-    player = input("Enter a choice (rock, paper, scissor): ")
-
-print(f"Player: {player}")
-print(f"computer: {computer}")
-
-if player == computer:
-    print("It's a tie!")
-elif player == "rock" and computer == "scissors":
-    print("You win!")
-elif player == "scissors" and computer == "rock":
-    print("You win!")
-elif player == "scissors" and computer == "paper":
-    print("You win!")
-else:
-    print("You Win!")
-
+    print("You lose!")
 
 import random
 
@@ -67,6 +43,30 @@ elif player == "scissors" and computer == "paper":
     print("You win!")
 else:
     print("You lose!")
+
+
+import random
+
+options = ("rock", "paper", "scissor")
+player = None
+computer = random.choice(options)
+
+while player not in options:
+    player = input("Enter a choice (rock, paper, scissor): ")
+
+print(f"Player: {player}")
+print(f"computer: {computer}")
+
+if player == computer:
+    print("It's a tie!")
+elif player == "rock" and computer == "scissors":
+    print("You win!")
+elif player == "scissors" and computer == "rock":
+    print("You win!")
+elif player == "scissors" and computer == "paper":
+    print("You win!")
+else:
+    print("You win!")
 
 
 print("thank for playing!")
